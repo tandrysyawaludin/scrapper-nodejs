70cea044b2b16efcc7011790dfa7e2a7c7f40f35
tandry <syawaludintandry@yahoo.com>
2019-03-25 22:20:23 +0700

    add write to json file and remove unused package

--- solution.js
+++ solution.js
@@ -3,66 +3,101 @@
 require('colors');
 const request = require('request');
 const cheerio = require('cheerio');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const baseUrl = 'https://www.bankmega.com/'
+const fs = require('fs');
 
-function init() {    
-    fetchCategories();
+let result = {};
+let subCategories = [];
+
+function init() {
+    categoriesCollector()
 }
 
-function fetchCategories() {    
-    const url = 'https://www.bankmega.com/promolainnya.php';
-    
-    request(url, function (err, res, body) {
-        if (err && res.statusCode !== 200) throw err;
-    
+function fetchCategories() {
+    const url = `${baseUrl}promolainnya.php`;
+
+    return new Promise((resolve, reject) => {
+        request(url, function (err, res, body) {
+            if (err && res.statusCode !== 200) throw err;
+
+            resolve(body);
+        });
+    })
+}
+
+async function categoriesCollector() {
+    const body = await fetchCategories()
+    const $ = cheerio.load(body);
+    let categories = [];
+
+    $('#subcatpromo').each((i, value) => {
+        $(value).find('img').each((j, data) => {
+            categories.push(data.attribs.id);                
+        });
+    });
+
+    detailLinksCollector(categories)
+}
+
+async function detailLinksCollector(categories) {    
+    await Promise.all(categories.map(async (category, key) => {
+        const url = `${baseUrl}ajax.promolainnya.php?product=0&subcat=${key + 1}`;
+        const body = await fetchDetailLinks(url)
         const $ = cheerio.load(body);
-        const dom = new JSDOM(body);
-        let categories = []
         
-        $('#subcatpromo').each((i, value) => {
-            $(value).find('img').each((j, data) => {
-                categories.push(data.attribs.id);
-                // https://www.bankmega.com/ajax.promolainnya.php?product=0&subcat=3
+        $('#promolain').each((i, value) => {
+            $(value).find('a').each((j, data) => {                                             
+                subCategoriesCollector(category, data.attribs.href)
             });
         });
-
-        fetchSubCategories(categories, $, dom)
-    });
+    }))    
 }
 
-function fetchSubCategories(categories, $, dom) {
-    let result = {};
-    // let subCategories = [];
-
-    categories.map((value, key) => {
-        dom.window.document.getElementById(value).click();
-        setTimeout(function(){ resultSetter(result, $, value) }, 5000);
-        // $('#promolain').each((i, value) => {
-        //     $(value).find('img').each((j, data) => {
-        //         subCategories[j] = {
-        //            title: data.attribs.title,
-        //            imageurl: data.attribs.src
-        //        }
-        //     });
-        // });
-        // result[value] = subCategories
-    })  
+function fetchDetailLinks(url) {
+    return new Promise((resolve, reject) => {
+        request(url, function (err, res, body) {
+            if (err && res.statusCode !== 200) throw err;
+            
+            resolve(body)
+        })
+    })
 }
 
-function resultSetter(result, $, category) {
-    let subCategories = [];
+async function subCategoriesCollector(category, link) {
+    const body = await fetchSubCategories(link);
+    const $ = cheerio.load(body);
+    const subCategory = {
+        title: $('.titleinside h3')[0]['children'][0]['data'],
+        image_url: `${baseUrl}${ $('.keteranganinside img')[0]['attribs']['src']}`,
+        area_promo: $('.area b')[0]['children'][0]['data'],
+        start_period: $('.periode b')[0]['children'][0]['data'].replace(/[^a-zA-Z0-9 ]/g, "").trim(),
+        end_period: $('.periode b')[1]['children'][0]['data'],
+        category: category
+    }
+    
+    resultSetter(category, subCategory)
+}
 
-    $('#promolain').each((i, value) => {
-        $(value).find('img').each((j, data) => {
-            subCategories[j] = {
-               title: data.attribs.title,
-               imageurl: data.attribs.src
-           }
-        });
-    });
+function fetchSubCategories(link) {
+    const url = `${baseUrl}${link}`;
+
+    return new Promise((resolve, reject) => {
+        request(url, function (err, res, body) {
+            if (err && res.statusCode !== 200) throw err;
+
+            resolve(body)            
+        })
+    })
+}
+
+function resultSetter(category, subCategory) {
+    subCategories = [...subCategories, subCategory]
     result[category] = subCategories
-    console.log(result);
+    
+    fs.writeFile ("solution.json", JSON.stringify(subCategories, null, 2), function(err) {
+        if (err) throw err;
+        console.log('complete');
+    });
 }
 
 init();
